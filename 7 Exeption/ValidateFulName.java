5cf2fa4459243c4a7d70f4814e22675e552f2c1e
nastyarndn <92645835+nastyarndn@users.noreply.github.com>
2021-12-11 01:15:28 +0600

    Update ValidateFulName.java

--- 7 Exeption/ValidateFulName.java
+++ 7 Exeption/ValidateFulName.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class ValidateFulName {
 
   public static int getLengthFullName(String fullName) throws Exception {
@@ -5,5 +8,18 @@ public class ValidateFulName {
       throw new Exception("Exception: fullName  is null!");
     }
     return fullName.length();
+  }
+
+  public static int getAge(LocalDate birthDate) throws Exception {
+    LocalDate now = LocalDate.now();
+    int age = Period.between(birthDate, now).getYears();
+    try {
+      if (age < 18) {
+        throw new Exception("Exception:  Age less than 18=> " + age);
+      }
+    } catch (Exception e) {
+      System.err.println("Age less than 18=> " + age);
+    }
+    return age;
   }
 }
