cf4b48b37b6038e9e44ac28f5b93fd38fed34780
nastyarndn <92645835+nastyarndn@users.noreply.github.com>
2021-12-11 01:11:44 +0600

    Add files via upload

--- 6 Bank/Bank.java
+++ 6 Bank/Bank.java
@@ -1,9 +1,10 @@
+import java.time.LocalDate;
 import java.util.*;
 import java.util.Map.Entry;
 
 public class Bank {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Account account1 = new Account(5, "A111");
     Account account2 = new Account(10, "A222");
     Account account3 = new Account(100, "B111");
@@ -21,24 +22,42 @@ public class Bank {
     List<Account> accForClient3 = new ArrayList<Account>();
     accForClient3.add(account5);
 
-    Client client1 = new Client("Nastya", 29);
-    Client client2 = new Client("Irina", 30);
-    Client client3 = new Client("Valeriya", 20);
+    LocalDate birthDateForClient1 = LocalDate.of(1998, 11, 28);
+    LocalDate birthDateForClient2 = LocalDate.of(1972, 9, 28);
+    LocalDate birthDateForClient3 = LocalDate.of(2002, 4, 22);
+
+    Client client1 = new Client("Nastya", birthDateForClient1);
+    Client client2 = new Client("Irina", birthDateForClient2);
+    Client client3 = new Client("Valeriya", birthDateForClient3);
 
     Map<Client, List<Account>> bank = new HashMap<>();
     bank.put(client1, accForClient1);
     bank.put(client2, accForClient2);
     bank.put(client3, accForClient3);
 
-    System.out.println("Search account for client: " + bank.get(client1)); // Находим значение по ключу client1
+    System.out.println( "Search account for client: " + getAccOnClient(client1, bank)); // Находим значение по ключу client1
+    System.out.println( "Search client for account: " + getClientOnAcc(account3, bank));
+   
+  }
 
+  private static List<Account> getAccOnClient(Client client, Map<Client, List<Account>> bank) {
+    List<Account> acc = bank.get(client);
+    return acc;
+  }
+
+  private static Client getClientOnAcc(Account account, Map<Client, List<Account>> bank) {
+   
     Set<Entry<Client, List<Account>>> entrySet = bank.entrySet();
     for (Map.Entry<Client, List<Account>> pair : entrySet) {
       for (Account pair2 : pair.getValue()) {
-        if (account4.equals(pair2)) {
-          System.out.println("Search client for account: " + pair.getKey()); // Находим ключ по счету account1
+        if (account.equals(pair2)) {
+          return pair.getKey(); 
         }
       }
     }
+    return null;
+
   }
+
+
 }
--- 6 Bank/Client.java
+++ 6 Bank/Client.java
@@ -1,15 +1,23 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 class Client {
 
   private String name;
-  private int age;
+  private LocalDate birthDate;
 
-  public Client(String name, int age) {
+  public Client(String name, LocalDate birthDate) throws Exception {
+    LocalDate now = LocalDate.now();
+    int age = Period.between(birthDate, now).getYears();
+    if (age < 18) {
+      throw new Exception("Exception: "+ name + " less than 18 years old => " + age);
+    } else 
+    this.birthDate = birthDate;
     this.name = name;
-    this.age = age;
   }
 
   @Override
   public String toString() {
-    return "Client [age=" + age + ", name=" + name + "]";
+    return "Client [birthDate=" + birthDate + ", name=" + name + "]";
   }
 }
